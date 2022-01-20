f33ecbaeaf3d99bade99e0f74da9f0193e13595e
emagweb <emagcodeshake@gmail.com>
2022-01-20 14:22:45 +0200

    change dir

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ import Amplify from 'aws-amplify';
 import App from './App';
 import "@aws-amplify/ui-react/styles.css";
 import { AmplifyProvider } from "@aws-amplify/ui-react";
-import awsconfig from './aws-exports';
+import awsconfig from './src/aws-exports';
 Amplify.configure(awsconfig);
 //import { Home } from './ui-components';
 
